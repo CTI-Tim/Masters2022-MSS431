950d0c5e0260dbb90630f86466be837491be424a
CTI-TimGray <79595771+CTI-Tim@users.noreply.github.com>
2022-08-03 11:01:43 -0400

    Initial Commit of Class Program

---
+++ CrestronMastersMSS431InstructorProgram/GetFortune/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GetFortune")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GetFortune")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f98a8c86-fb11-4dad-8ccf-cbb6b78e01ee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CrestronMastersMSS431InstructorProgram/ReflectionInterface/ReflectionInterface.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReflectionInterface
+{
+    /// <summary>
+    /// This is the interface template we are going to use for our reflection
+    /// This interface MUST be used in your program and in all libraries you intend to reflect into your
+    /// program that are going to be based on this interface.  Using an Interface will significantly simplify
+    /// your use of reflection.  Without an interface you must dig into the dll and discover everything.
+    /// 
+    /// This interface MUST be referenced in your program and your libraries that will be based on it
+    /// </summary>
+    public interface IpluginInterface
+    {
+
+        event EventHandler<DeviceEventArgs> DeviceEvent;
+
+        void Random();
+        void Next();
+        void Previous();
+    }
+    // Also define any event classes here in the interface to ensure they are
+    // consistent across all libraries you use the interface in
+    public class DeviceEventArgs : EventArgs
+    {
+        public DeviceEventArgs(string message)
+        {
+            Message = message;
+        }
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+}
